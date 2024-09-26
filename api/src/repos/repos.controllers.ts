a49607e1db2777d82395eb2b7e688a2562edc301
Gadln <helene.gadessaud@icloud.com>
2024-09-26 11:08:07 +0200

    Add road POST for repo

--- api/src/repos/repos.controllers.ts
+++ api/src/repos/repos.controllers.ts
@@ -1,11 +1,43 @@
-import express, { Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import repos from '../../data/repos.json';
-//import type { Repo } from "./repo.type";
+import type { Repo } from "./repo.type";
+import Joi from "joi";
 
 const repoControllers = express.Router();
 
+const schema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  url: Joi.string().required(),
+  isPrivate: Joi.number().min(1).max(2).required()
+})
+
+
+const validateRepo = (req: Request, res: Response, nest : NextFunction) =>{
+  const { error } = schema.validate(req.body)
+  if (error == null){
+    nest()
+  }else {
+    res.status(422).json(error) // info sur l'erreur
+  }
+}
+
 repoControllers.get('/', (_: any, res: Response) => {
   res.status(200).json(repos)
+})
+
+repoControllers.get('/:id', (req: Request, res: Response) => {
+  const repo = repos.find(rep => rep.id === req.params.id) as Repo;
+  if (repo) {
+    res.status(200).json(repo)
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+repoControllers.post('/', validateRepo, (req: Request, res: Response) => {
+  repos.push(req.body)
+  res.status(201).json(req.body)
 })
 
 export default repoControllers;
