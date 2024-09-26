a75b20c96b954060962c367a4c163ad31c068209
Gadln <helene.gadessaud@icloud.com>
2024-09-26 11:54:29 +0200

    Add road /langs/:id

--- api/src/langs/langs.controllers.ts
+++ api/src/langs/langs.controllers.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import langs from "../../data/langs.json";
+import type { Lang } from './lang.type';
 import Joi from "joi";
 
 const langsControllers = express.Router();
@@ -22,6 +23,16 @@ const validateLang = (req: Request, res:Response, next: NextFunction) => {
 langsControllers.get('/', (_: any, res: Response) => {
   res.status(200).json(langs)
 })
+
+langsControllers.get('/:id', (req: Request, res: Response)=> {
+  const lang = langs.find(lg => lg.id === parseInt(req.params.id)) as Lang;
+if (lang) {
+  res.status(200).json(lang)
+}else {
+  res.sendStatus(404)
+}
+} 
+)
 
 langsControllers.post('/', validateLang, (req: Request, res: Response)=> {
   langs.push(req.body)
