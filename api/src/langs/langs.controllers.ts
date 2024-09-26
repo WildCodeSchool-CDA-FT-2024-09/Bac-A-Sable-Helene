f33f45ccea7a80559ea4d2c7d1b7dad060e00b94
Gadln <helene.gadessaud@icloud.com>
2024-09-26 11:41:35 +0200

    Add road POST for languages

--- api/src/langs/langs.controllers.ts
+++ api/src/langs/langs.controllers.ts
@@ -1,13 +1,33 @@
-import express, { Response } from "express";
-import langs from "../../data/repos.json";
+import express, { Request, Response, NextFunction } from "express";
+import langs from "../../data/langs.json";
+import Joi from "joi";
 
-const langsController = express.Router();
+const langsControllers = express.Router();
 
-langsController.get('/', (_: any, res: Response) => {
+const schema = Joi.object({
+  id: Joi.number().required(),
+  name: Joi.string().required()
+})
+
+const validateLang = (req: Request, res:Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body)
+  if (error == null) {
+    next()
+  } else {
+    res.status(422).json(error)
+  }
+}
+
+
+langsControllers.get('/', (_: any, res: Response) => {
   res.status(200).json(langs)
 })
 
+langsControllers.post('/', validateLang, (req: Request, res: Response)=> {
+  langs.push(req.body)
+  res.status(201).json(req.body)
+})
 
-export default langsController;
+export default langsControllers;
 
 
--- api/src/router.ts
+++ api/src/router.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { Response } from "express";
 import repoControllers from "./repos/repos.controllers";
-import langsController from "./langs/langs.controllers";
+import langsControllers from "./langs/langs.controllers";
 import statusControllers from "./status/status.controllers";
 import repoLangControllers from "./repoLang/repoLang.controllers";
 
 const router = express.Router();
 
 router.use('/repos', repoControllers);
-router.use('/langs', langsController);
+router.use('/langs', langsControllers);
 router.use('/status', statusControllers);
 router.use('/repoLang', repoLangControllers);
 
