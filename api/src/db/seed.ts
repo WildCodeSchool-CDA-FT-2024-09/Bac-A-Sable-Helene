a8ef52ad35a63f79865651fd48680d0898a4399a
Gadln <helene.gadessaud@icloud.com>
2024-10-02 12:12:45 +0200

    Add repo from repos.json with status and languages associated

--- api/src/db/seed.ts
+++ api/src/db/seed.ts
@@ -6,6 +6,10 @@ import langs from "../../data/langs.json";
 import { Status } from "../status/status.entities";
 import status from "../../data/status.json";
 
+import { Repo } from "../repos/repo.entities";
+import repos from "../../data/repos.json";
+import repoLang from "../../data/repoLang.json"
+
 (async() => {
   await dataSource.initialize();
   const queryRunner = dataSource.createQueryRunner();
@@ -39,6 +43,31 @@ import status from "../../data/status.json";
     )
 
     console.log(savedStatus)
+
+    const savedRepos = await Promise.all(
+      repos.map(async (el) => {
+        const repo = new Repo();
+        repo.id = el.id;
+        repo.name = el.name;
+        repo.url = el.url;
+
+        const status = savedStatus.find((st) => st.id === el.isPrivate) as Status;
+        repo.status = status;
+
+        const mylangs = savedlangs.filter((svLg) => {
+          console.log("repoID", el.id)
+          const associatedlang = repoLang.filter(lgbyrep => lgbyrep.repo_id === el.id);
+          console.log("A", associatedlang)
+          const langLabel = langs.filter(lg => associatedlang.some((assolg) => assolg.lang_id === lg.id))
+          return langLabel.some(lgLabel => lgLabel.name === svLg.name)
+        })
+        repo.languages = mylangs;
+
+        return await repo.save();
+      })
+    )
+
+    console.log(savedRepos)
 
     await queryRunner.commitTransaction();
 
