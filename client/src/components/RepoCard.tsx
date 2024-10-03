6e0a8864b009b60b4ce5e1039e6a9a1c4af9a1fd
Gadln <helene.gadessaud@icloud.com>
2024-10-03 16:49:45 +0200

    Add CSS

--- client/src/components/RepoCard.tsx
+++ client/src/components/RepoCard.tsx
@@ -3,14 +3,20 @@ import "./styleComponent.css";
 
 function RepoCard({ name, url, status, languages }: Repo){
   // Déterminer la couleur en fonction du status.id
-  const statusColor = status.label === 'Public' ? 'green' : 'red'; // Vert si Public, Rouge si Private
+  const statusClass = status.label === 'Public' ? 'public' : 'private'; // Vert si Public, Rouge si Private
 
 return (
   <div className="card">
     <h2 className="title">{name} - {status.label}</h2>
-    <h3 style= {{color: statusColor}}>{status.label}</h3>
-    <h3 className="url">{url}</h3>
-    <div>Languages:
+    <h3 className={statusClass}>{status.label}</h3>
+    <a className="url" href={url} target="_blank" rel="noopener noreferrer">{url}</a>
+    {/* href={url} : Définit l'URL vers laquelle le lien pointe.
+
+        target="_blank" : Ouvre le lien dans un nouvel onglet. C'est une pratique courante pour les liens externes.
+        
+        rel="noopener noreferrer" : Améliore la sécurité en empêchant la nouvelle page d'accéder à la page d'origine et aide également à prévenir les attaques de type phishing. Cela est particulièrement important lorsque tu utilises target="_blank".
+  */}
+    <div className="languages">Languages:
      <ul className="langsList">
       {languages.map((lang: Lang) => (
         <li key={lang.name} className="langTag">{lang.name}</li>
