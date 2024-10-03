eba45a10695969c5412c1e05515ca71cc3823817
Gadln <helene.gadessaud@icloud.com>
2024-10-03 14:32:15 +0200

    Connexion front - back with cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,10 +3,17 @@ import express from 'express';
 import router from './router';
 import dotenv from 'dotenv';
 import { dataSource } from "./db/client";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL as string,
+  })
+);
 app.use(express.json());  // MDW pour lire les fichiers json
 
 app.use('/api', router);
