fe9e183e82b0df7f7f0ea2150d72178714755a00
Gadln <helene.gadessaud@icloud.com>
2024-10-09 18:51:09 +0200

    change roads for repos and languages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { Repo } from './types/RepoType';
 import RepoCard from './components/RepoCard';
 import { useQuery } from "@apollo/client";
 import GET_REPOS from './services/GET_REPOS';
 import GET_LANGS from './services/GET_LANGS';
 
+import { useLocation } from 'react-router-dom';
+
 function App() {
+  const location = useLocation();
+
   // State pour gérer l'affichage des repos ou des langues
   const [view, setView] = useState<'repos' | 'languages'>('repos');
+
+  // Effet pour mettre à jour la vue basée sur l'URL
+  useEffect(() => {
+    if (location.pathname === '/languages') {
+      setView('languages');
+    } else {
+      setView('repos');
+    }
+  }, [location.pathname]); // Dépendance sur l'URL
+
 
   // Requête pour les repos
   const { loading: loadingRepos, error: errorRepos, data: reposData, refetch: refetchRepos } = useQuery(GET_REPOS);
@@ -20,12 +34,18 @@ function App() {
   if (loadingRepos || loadingLangs) return <h1>Loading ...</h1>;
   if (errorRepos || errorLangs) return <p>Error</p>;
 
+
   return (
     <>
       <h1 className="titleH1">Mes Repos GitHub</h1>
       <nav className="navbar">
-        <button className="nav-link" onClick={() => setView('repos')}>Tous les Repos</button>
-        <button className="nav-link" onClick={() => setView('languages')}>Tous les Languages</button>
+      <button className="nav-link" onClick={() => setView('repos')}>
+          <a href="/repos">Tous les Repos</a>
+        </button>
+
+        <button className="nav-link" onClick={() => setView('languages')}>
+          <a href="/languages">Tous les Langues</a>
+        </button>
       </nav>
 
       <main className="main">
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,30 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
-// import connexion from "./services/connexion.ts";
 import client from "./services/connexion.ts";
+
+import App from "./App.tsx";
+
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/repos",
+    element: <App />,
+  },
+  {
+    path: "/languages",
+    element: <App />,  
+  },
+
+
+]);
+
 
 //Le point d'exclamation est pour rendre l'affichage du root (dsn index.html) obligatoire sinon rien ne s'affiche.
 createRoot(document.getElementById('root')!).render(
